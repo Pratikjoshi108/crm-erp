0616c4a83c6b065fb2b278f6b8f1d713bf8b8a14
Pratik Joshi <p88140j@gmail.com>
2026-09-22 17:02:39 +0530

    Fix production Socket.IO URL

--- client/src/useNotifications.js
+++ client/src/useNotifications.js
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { apiRequest } from "./api";
 
-const SOCKET_URL = "http://localhost:5000";
+const SOCKET_URL =
+  import.meta.env.VITE_SOCKET_URL || "http://localhost:5000";
 
 function getUserIdFromToken(token) {
   try {
